18b7e5d7995ce4ae05ab8f6ce2de8159920ffb0f
Sara Yue Madruga Martín <smm1032@alu.ubu.es>
2026-04-20 19:58:41 +0200

    First Commit

---
+++ Pr02_SaraYueMadrugaMartin/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Pr02_SaraYueMadrugaMartin
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
