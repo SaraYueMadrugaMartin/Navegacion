a0f97104f447be5a8a77f548dfb2605ddff009b9
Sara Yue Madruga Martín <smm1032@alu.ubu.es>
2026-04-22 19:08:28 +0200

    Cambio tamaños fuente

--- Pr02_SaraYueMadrugaMartin/SettingsPage.xaml.cs
+++ Pr02_SaraYueMadrugaMartin/SettingsPage.xaml.cs
@@ -24,4 +24,31 @@ public partial class SettingsPage : ContentPage
         // Guardar la fuente seleccionada.
         Preferences.Set("font", font);
     }
+
+    /// <summary>
+    /// Metodo que cambia el tamaño global de la fuente de la acplicacion de manera dinamica.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private void ChangeFontSize(object sender, EventArgs e)
+    {
+        var option = ((Picker)sender).SelectedItem?.ToString();
+
+        // Se asigna un numero como multiplicador para cada opción de tamaño elegido.
+        double scale = option switch
+        {
+            "Pequeño" => 0.5,
+            "Normal" => 1.0,
+            "Grande" => 2,
+            _ => 1.0
+        };
+
+        // Se recalculan los tamaños de la fuentes globales con la escala corresponiente esocgida.
+        Application.Current.Resources["FontTitle"] = 32 * scale;
+        Application.Current.Resources["FontSubtitle"] = 22 * scale;
+        Application.Current.Resources["FontBody"] = 16 * scale;
+
+        // Se guarda el tamaño escogido.
+        Preferences.Set("scale", scale);
+    }
 }
